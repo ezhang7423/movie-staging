b61d266681bbae3429736b99b6f15da832ed3abb
ezhang7423 <ezhang7423@student.palomar.edu>
2022-01-04 18:12:19 -0800

    clean up

--- src/App.js
+++ src/App.js
@@ -1,58 +1,13 @@
 import * as THREE from 'three'
-import React, { Suspense, useEffect, useState, useRef, forwardRef, useCallback } from 'react'
+import React, { Suspense, useEffect, useState, forwardRef } from 'react'
+import useMuteWithRefCallback from './mute'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Reflector, Text, useTexture, useGLTF } from '@react-three/drei'
 import Overlay from './Overlay'
 
-const isSafari = () => {
-  const ua = navigator.userAgent.toLowerCase()
-  return ua.indexOf('safari') > -1 && ua.indexOf('chrome') < 0
-}
-
 function Carla(props) {
   const { scene } = useGLTF('carla-draco.glb')
   return <primitive object={scene} {...props} />
-}
-function useMuteWithRefCallback() {
-  const ref = useRef(null)
-  const setRef = useCallback((node) => {
-    // Save a reference to the node
-    ref.current = node
-
-    if (isSafari() && ref.current) {
-      // obtain reference to the video element
-      const player = ref.current.image
-      // if the reference to video player has been obtained
-      if (player) {
-        console.log(player)
-        // set the video attributes using javascript as per the
-        // webkit Policy
-        player.controls = false
-        player.playsinline = true
-        player.muted = true
-        player.setAttribute('muted', '') // leave no stones unturned :)
-        player.autoplay = true
-
-        // Let's wait for an event loop tick and be async.
-        setTimeout(() => {
-          // player.play() might return a promise but it's not guaranteed crossbrowser.
-          const promise = player.play()
-          // let's play safe to ensure that if we do have a promise
-          if (promise.then) {
-            promise
-              .then(() => {})
-              .catch(() => {
-                // if promise fails, hide the video and fallback to <img> tag
-                ref.current.style.display = 'none'
-                setShouldUseImage(true)
-              })
-          }
-        }, 0)
-      }
-    }
-  }, [])
-
-  return [setRef]
 }
 
 const VideoText = forwardRef(({ clicked, ...props }, ref) => {
