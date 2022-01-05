77f8b982a7f68c6cfc33c3e8b62ee82c48246d32
ezhang7423 <ezhang7423@student.palomar.edu>
2022-01-04 18:09:10 -0800

    fix safari

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import * as THREE from 'three'
-import React, { Suspense, useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState, useRef, forwardRef, useCallback } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Reflector, Text, useTexture, useGLTF } from '@react-three/drei'
 import Overlay from './Overlay'
+
+const isSafari = () => {
+  const ua = navigator.userAgent.toLowerCase()
+  return ua.indexOf('safari') > -1 && ua.indexOf('chrome') < 0
+}
 
 function Carla(props) {
   const { scene } = useGLTF('carla-draco.glb')
   return <primitive object={scene} {...props} />
 }
+function useMuteWithRefCallback() {
+  const ref = useRef(null)
+  const setRef = useCallback((node) => {
+    // Save a reference to the node
+    ref.current = node
 
-function VideoText({ clicked, ...props }) {
-  const [video] = useState(() => Object.assign(document.createElement('video'), { src: 'eddie.mp4', crossOrigin: 'Anonymous', loop: false, muted: true }))
+    if (isSafari() && ref.current) {
+      // obtain reference to the video element
+      const player = ref.current.image
+      // if the reference to video player has been obtained
+      if (player) {
+        console.log(player)
+        // set the video attributes using javascript as per the
+        // webkit Policy
+        player.controls = false
+        player.playsinline = true
+        player.muted = true
+        player.setAttribute('muted', '') // leave no stones unturned :)
+        player.autoplay = true
+
+        // Let's wait for an event loop tick and be async.
+        setTimeout(() => {
+          // player.play() might return a promise but it's not guaranteed crossbrowser.
+          const promise = player.play()
+          // let's play safe to ensure that if we do have a promise
+          if (promise.then) {
+            promise
+              .then(() => {})
+              .catch(() => {
+                // if promise fails, hide the video and fallback to <img> tag
+                ref.current.style.display = 'none'
+                setShouldUseImage(true)
+              })
+          }
+        }, 0)
+      }
+    }
+  }, [])
+
+  return [setRef]
+}
+
+const VideoText = forwardRef(({ clicked, ...props }, ref) => {
+  const [video] = useState(() => Object.assign(document.createElement('video'), { src: 'eddie.mp4', crossOrigin: 'Anonymous', loop: false }))
   useEffect(() => void (clicked && video.play()), [video, clicked])
   return (
     <Text font="/Inter-Bold.woff" fontSize={3} letterSpacing={-0.06} {...props}>
       eddie
       <meshBasicMaterial toneMapped={false}>
-        <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
+        <videoTexture attach="map" args={[video]} ref={ref} encoding={THREE.sRGBEncoding} />
       </meshBasicMaterial>
     </Text>
   )
-}
+})
 
 function Ground() {
   const [floor, normal] = useTexture(['SurfaceImperfections003_1K_var1.jpg', 'SurfaceImperfections003_1K_Normal.jpg'])
@@ -34,6 +80,7 @@ function Ground() {
 export default function App() {
   const [clicked, setClicked] = useState(false)
   const [ready, setReady] = useState(false)
+  const [ref] = useMuteWithRefCallback()
   const store = { clicked, setClicked, ready, setReady }
   return (
     <>
@@ -43,7 +90,7 @@ export default function App() {
         <Suspense fallback={null}>
           <group position={[0, -1, 0]}>
             <Carla rotation={[0, Math.PI - 0.4, 0]} position={[-1.2, 0, 0.6]} scale={[0.26, 0.26, 0.26]} />
-            <VideoText {...store} position={[0, 1.3, -2]} />
+            <VideoText {...store} ref={ref} position={[0, 1.3, -2]} />
             <Ground />
           </group>
           <ambientLight intensity={0.5} />
